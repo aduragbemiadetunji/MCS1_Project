0b0900589f149f4ace20976797022e24c96be83e
Marius Vik <marius.vik01@gmail.com>
2023-10-30 18:41:24 +0100

    finished (?) needs testing with ship model

--- Part2/init.m
+++ Part2/init.m
@@ -18,22 +18,29 @@ eta0 = [0,0,0,0,0,0]';
 % Initial velocity u, v, w, p, q, r
 nu0 = [0,0,0,0,0,0]';
 
+%Simulation time
+N = 500;
 
 %% Wind constants/parameters
 
+%Set to 1 to turn on wind effect
+wind = 1;
+
+%Wind direction
 direction_limit = pi/36;
-slow_varying_limit = 1;
-mean_wind_direction = 0;
-direction_time_constant = 10;
+mean_wind_direction = pi/2;
 np_direction = 0.1;
+direction_time_constant = 100;
+
+%Slow varying wind
+slow_varying_limit = 1;
 np_slow_varying = 5;
+slow_varying_time_constant = 100;
+
+%Wind gust and mean wind component
 u10 = 12;
 height = 3;
 kappa = 0.003;
-N = 500;
-slow_varying_time_constant = 100;
-phases_min = -pi*ones(N,1);
-phases_max = pi*ones(N,1);
-
-L = 180;
+L = 1000;
 kappa2 = 0.0026;
+gust_limit = 40;
