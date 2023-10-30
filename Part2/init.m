67e65f930357dfba80a5b19504233e99d08d03a7
AyonRRahman <raqibur.ayon@gmail.com>
2023-10-30 14:38:28 +0100

    EKF needs tuning

--- Part2/init.m
+++ Part2/init.m
@@ -7,7 +7,7 @@
 %                                                                         %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%clear all;
+clear all;
 
 load('supply.mat');
 load('supplyABC.mat');
@@ -44,6 +44,9 @@ change_position_time = 400;
 %% PID Parameters
 Mass = diag([vesselABC.MRB(1,1), vesselABC.MRB(2,2), vesselABC.MRB(6,6)]); %vesselABC.MRB(1:3,1:3);
 Damping = diag([2.6486e5, 8.8164e5, 3.3774e8]);
+
+M = Mass;
+D = Damping;
 T_n = 70; %period of interest -- can tune
 omega_n_dp = 2* pi / T_n;
 zeta = 0.7; %--- can tune
@@ -80,11 +83,54 @@ Kd = [1660451.40544401; 3920391.80535684; 1605063439.04156];
 
 
 %observer
-ws_var.init_state = zeros(15,1);
-ws_var.init_Pk = 5*eye(15); %tune later
+
+K_w = diag([1, 1, 1]); %-------TUNE
+Ew = [zeros(3); K_w];
+Eb = diag([1, 1, 1]); %-----TUNE 
+E = [Ew, zeros(6,3);
+            zeros(3,3), zeros(3,3);
+            zeros(3,3), Eb;
+            zeros(3,3),zeros(3,3)];
+
+dt = 0.01;
+TAU = dt*E;E = [Ew, zeros(6,3);
+            zeros(3,3), zeros(3,3);
+            zeros(3,3), Eb;
+            zeros(3,3),zeros(3,3)];
+
+obs.Q = diag([1,1,0.1,1e4,1e4,1e5]);
+
+B =  [zeros(6,3);zeros(3,3);zeros(3,3);inv(M)];
+R = diag([1,1,0.1]); %tune later
+Q = diag([1,1,0.1,1e2,1e2,1e2])*10e26; %tune this
 
 
+Cw = [zeros(3) eye(3)];
+H = [Cw, eye(3), zeros(3,3) zeros(3,3)];
 
+T_n = 20; %period of interest -- can tune
+omega_n = 2* pi / T_n;
+zetas = 0.1; %--- can tune
+Omega = diag([omega_n, omega_n, omega_n]);
+Caret = diag([zetas, zetas, zetas]);
+Tb = 0.1 * eye(3); %tune the 0.1
+inv_Tb = inv(Tb);
+Aw =  [zeros(3) eye(3);
+    -Omega^2 -2*Caret*Omega];
+
+K_w = diag([1, 1, 1]); %-------TUNE
+Ew = [zeros(3); K_w];
+
+Eb = diag([1, 1, 1]); %-----TUNE
+E = [Ew, zeros(6,3);
+            zeros(3,3), zeros(3,3);
+            zeros(3,3), Eb;
+            zeros(3,3),zeros(3,3)];
+
+
+inv_M = inv(M);
+dt = 0.1;
+TAU = dt*E;
 % plot(simout.Data(:,1),simout.Data(:,2))
 
 
